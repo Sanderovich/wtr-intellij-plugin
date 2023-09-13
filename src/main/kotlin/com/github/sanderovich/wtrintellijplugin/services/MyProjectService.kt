3ef75a80dc442930cb01ea15b34eff4670020961
GitHub Action <action@github.com>
2023-09-13 15:16:13 +0000

    Template cleanup

--- src/main/kotlin/com/github/sanderovich/wtrintellijplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/sanderovich/wtrintellijplugin/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.sanderovich.wtrintellijplugin.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.sanderovich.wtrintellijplugin.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
